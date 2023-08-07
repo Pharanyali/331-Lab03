7cf0551501b8663617ccb2b7e163f9a0cf5582cb
Pharanya <pharanya_li@cmu.ac.th>
2023-08-07 19:01:49 +0700

    4.2

--- real-world-vue/src/views/NetworkErrorView.vue
+++ real-world-vue/src/views/NetworkErrorView.vue
@@ -7,7 +7,7 @@ const router = useRouter()
         <h1>Uh-oh!</h1>
         <h3>
             It look like you're experiencing some network issues, please take a breath and
-            <a href="#" @click="router.go(-1)">click here</a>
+            <a href="/" @click="router.go(-1)">click here</a>
             to try again.
         </h3>
         <p>Sorry, we can't get the data from the server.</p>
